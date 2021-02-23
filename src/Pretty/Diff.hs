101f50c48f4d675717af6ed443dfc49a5ebb9d79
Stoeffel <schtoeffel@gmail.com>
2021-02-23 12:58:51 +0100

    More cleanup

--- src/Pretty/Diff.hs
+++ src/Pretty/Diff.hs
@@ -106,48 +106,38 @@ data MultilineContext
 -- | Printing a full diff of both values separated by some pipes.
 pretty :: Config -> Text -> Text -> Text
 pretty Config {separatorText, wrapping, multilineContext} x y =
-  let xs = Text.lines x
-      ys = Text.lines y
-   in [ zipWithSameLength above' xs ys
-          & map (\(x, y) -> (x, wrap wrapping y))
-          & extractContext multilineContext (False, [], [])
-          & Text.unlines
-          & Text.dropAround (== '\n'),
-        separator separatorText,
-        zipWithSameLength below' xs ys
-          & map (\(x, y) -> (x, wrap wrapping y))
-          & extractContext multilineContext (False, [], [])
-          & Text.unlines
-          & Text.dropAround (== '\n')
-      ]
-        & mconcat
-
-zipWithSameLength :: (Maybe a -> Maybe a -> b) -> [a] -> [a] -> [b]
-zipWithSameLength f [] ys = map (\y -> f Nothing (Just y)) ys
-zipWithSameLength f xs [] = map (\x -> f (Just x) Nothing) xs
-zipWithSameLength f (x : xs) (y : ys) = f (Just x) (Just y) : zipWithSameLength f xs ys
+  mconcat
+    [ above wrapping multilineContext x y,
+      separator separatorText,
+      below wrapping multilineContext x y
+    ]
 
 -- | Printing The first value and the diff indicator above.
 --
 --  @
---  Diff.above Diff.NoWrap Diff.FullContext "1234" "_23"
+--  Diff.above Diff.NoWrap Diff.FullContext Diff.FullContext "1234" "_23"
 --  @
 --
 --  @
 --  ▼ ▼
 -- "1234"
 --  @
-above :: Wrapping -> Text -> Text -> Text
-above wrapping x y =
-  let (_, res) = above' (Just x) (Just y)
-   in wrap wrapping res
+above :: Wrapping -> MultilineContext -> Text -> Text -> Text
+above wrapping multilineContext x y =
+  let xs = Text.lines x
+      ys = Text.lines y
+   in sameLength xs ys
+        & map ((\(x, y) -> (x, wrap wrapping y)) . above')
+        & extractContext multilineContext (False, [], [])
+        & Text.unlines
+        & Text.dropAround (== '\n')
 
-above' :: Maybe Text -> Maybe Text -> (Bool, [Text])
-above' Nothing (Just y) =
+above' :: (Maybe Text, Maybe Text) -> (Bool, [Text])
+above' (Nothing, Just y) =
   (True, withDiffLine First down (map Diff.Second $ Text.unpack y))
-above' (Just x) Nothing =
+above' (Just x, Nothing) =
   (True, withDiffLine First down (map Diff.First $ Text.unpack x))
-above' (Just x) (Just y) =
+above' (Just x, Just y) =
   let diffs =
         Diff.getDiff
           (Text.unpack x)
@@ -159,28 +149,33 @@ above' (Just x) (Just y) =
 -- | Printing The second value and the diff indicator below.
 --
 --  @
---  Diff.below Diff.NoWrap "1234" "_23"
+--  Diff.below Diff.NoWrap Diff.FullContext "1234" "_23"
 --  @
 --
 --  @
 -- "_23"
 --  ▲
 --  @
-below :: Wrapping -> Text -> Text -> Text
-below wrapping x y =
-  let (_, res) = below' (Just x) (Just y)
-   in wrap wrapping res
+below :: Wrapping -> MultilineContext -> Text -> Text -> Text
+below wrapping multilineContext x y =
+  let xs = Text.lines x
+      ys = Text.lines y
+   in sameLength xs ys
+        & map ((\(x, y) -> (x, wrap wrapping y)) . below')
+        & extractContext multilineContext (False, [], [])
+        & Text.unlines
+        & Text.dropAround (== '\n')
 
-below' :: Maybe Text -> Maybe Text -> (Bool, [Text])
-below' Nothing (Just y) =
+below' :: (Maybe Text, Maybe Text) -> (Bool, [Text])
+below' (Nothing, Just y) =
   ( True,
     withDiffLine Second up (map Diff.Second $ Text.unpack y)
   )
-below' (Just x) Nothing =
+below' (Just x, Nothing) =
   ( True,
     withDiffLine Second up (map Diff.First $ Text.unpack x)
   )
-below' (Just x) (Just y) =
+below' (Just x, Just y) =
   let diffs =
         Diff.getDiff
           (Text.unpack x)
@@ -292,3 +287,8 @@ interleaveLists = mconcat . transpose
 
 filterEmptyLines :: [Text] -> [Text]
 filterEmptyLines = filter (not . Text.null . Text.strip)
+
+sameLength :: [a] -> [a] -> [(Maybe a, Maybe a)]
+sameLength [] ys = map (\y -> (Nothing, Just y)) ys
+sameLength xs [] = map (\x -> (Just x, Nothing)) xs
+sameLength (x : xs) (y : ys) = (Just x, Just y) : sameLength xs ys
